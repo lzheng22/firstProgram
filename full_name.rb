1f9fa1c9713b358d29330cc66dcbc29f4a9d7dca
Ubuntu <ubuntu@ip-172-31-84-139.ec2.internal>
2019-10-29 03:48:49 +0000

    second commit

---
+++ full_name.rb
@@ -0,0 +1,9 @@
+puts "what's your first name?"
+first = gets.chomp
+puts "what's your middle name?"
+middle = gets.chomp
+puts "what's your last name?"
+last = gets.chomp
+myarray = [first,middle,last]
+name = myarray.inspect
+puts "Hello #{name}!"
